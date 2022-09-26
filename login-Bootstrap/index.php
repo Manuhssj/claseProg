be6c53a43c536443d25d83e5e855581bbfc7a728
Manuel Alejandro Diaz Hernandez <109549833+Manuhssj@users.noreply.github.com>
2022-09-26 07:53:19 -0600

    Add files via upload

---
+++ login-Bootstrap/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <title>Login Form</title>
+</head>
+<body class="m-0 vh-100 row justify-content-center align-items-center">
+    <div class="card d-flex container-fluid w-25 ">
+        <h1 class="text-center mt-3">Login</h1>
+        <div class="form-floating mb-3 mt-4">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+            <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+        </div>
+        <button class="btn btn-primary mt-3 mb-2">Login</button>
+    </div>
+</body>
+</html>
