1bcc63d28ef2bd450adf9d1c16f9c1e02db3b764
Manuel Alejandro Diaz Hernandez <109549833+Manuhssj@users.noreply.github.com>
2022-10-03 22:48:37 -0600

    Add files via upload
    implementacion cargar imagen

---
+++ login-Bootstrap/body.templete.php
@@ -0,0 +1,135 @@
+<?php
+    include_once '..\auth\productController.php';
+    session_start();
+    $productos = new ProductController();
+    $productos = $productos -> chargeProducts($_SESSION["token"]);
+
+
+    // echo('<pre>');
+    // var_dump($productos);
+    // echo('</pre>');
+?>
+
+
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2 d-none d-sm-block ">
+                <aside class="bg-light">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="http://localhost/login-Boostrap/products/products.php">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                          </a>
+                          <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                          </ul>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link disabled">Disabled</a>
+                        </li>
+                      </ul>
+                </aside>
+            </div>
+            <div class="col-lg-10">
+                <div class="row d-flex flex-row justify-content-between mt-2 border-bottom">
+                    <div class="col mt-4 mb-4">
+                        <h1>Productos</h1>
+                    </div>
+                    <div class="col-2"><button class="btn btn-info mb-4 mt-4" data-bs-toggle="modal" data-bs-target="#addModal">Añadir producto</button></div>
+                </div>
+                <div class="row">
+                    <?php foreach($productos as $producto): ?>
+                      <div class="col-sm-3 col-md-3 col-lg-3 mb-5">
+                        <div class="card bg-light " style="width: 20rem;">
+                            <img src="<?php echo $producto['cover'] ?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title "><?php echo "<strong> " . $producto['name'] . " </strong> " ?></h5>
+                              <p class="card-text "> <?php echo $producto['description'] ?> </p>
+                                <div class="row">
+
+                                    <div class="col">
+                                        <a href="#" class="btn btn-warning w-100" data-bs-toggle="modal" data-bs-target="#editModal">Editar</a>
+                                    </div>
+
+                                    <div class="col">
+                                        <a href="#" class="btn btn-danger w-100" onclick="remove()">Eliminar</a>
+                                    </div>
+
+                                    <a href="detalleProducto.php" class="btn btn-info mt-2" >Detalles</a>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal Añadir</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form action="../auth/productController.php" method="POST" enctype="multipart/form-data">
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1">Name</span>
+                  <input type="text" class="form-control" placeholder="Name" name="name" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1">Slug</span>
+                  <input type="text" class="form-control" placeholder="Slug" name="slug" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1">Description</span>
+                  <input type="text" class="form-control" placeholder="Description" name="description" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1">Features</span>
+                  <input type="text" class="form-control" placeholder="Features" name="features" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1">Brand_Id</span>
+                  <input type="text" class="form-control" placeholder="Brand_Id" name="brand_id" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                  <input type="file" class="form-control" name="uploadedfile" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Save changes</button>
+                  <input type="hidden" name="action" value="create" >
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal Editar</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
---
+++ login-Bootstrap/productController.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+if(isset($_POST["action"])){
+  switch($_POST["action"]){
+    case 'create':
+      $name=strip_tags($_POST['name']);
+      $slug=strip_tags($_POST['slug']);
+      $description=strip_tags($_POST['description']);
+      $features=strip_tags($_POST['features']);
+      $brand_id=strip_tags($_POST['brand_id']);
+      
+      $controladorNew = new ProductController();
+      $image = $controladorNew->chargeImg($_FILES['uploadedfile']);
+      $controladorNew -> createProduct($name,$slug,$description,$features,$brand_id, $image);
+      break;
+
+  }
+}
+
+class ProductController {
+
+    public function chargeImg($img){
+      
+      $target_path  = "../img";
+      $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
+      if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+          echo "El archivo ".  basename( $_FILES['uploadedfile']['name']). 
+          " ha sido subido";
+      } else{
+          echo "Ha ocurrido un error, trate de nuevo!";
+      }
+      return $target_path;
+    }
+
+    public function createProduct($name,$slug,$description,$features,$brand_id, $image){
+
+      $curl = curl_init();
+      $token = $_SESSION["token"];
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('name' => $name,'slug' => $slug,'description' => $description, 'features' => $features,'brand_id' => $brand_id,'cover'=> new CURLFile($image)),
+        CURLOPT_HTTPHEADER => array(
+          "Authorization: Bearer $token"
+        ),
+      ));
+
+      $response = curl_exec($curl);
+      curl_close($curl);
+      $response= json_decode($response);
+      
+    }
+    public function chargeProducts($token){
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://crud.jonathansoto.mx/api/products',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_HTTPHEADER => array(
+            "Authorization: Bearer $token"
+            
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        
+        curl_close($curl);
+        $response = json_decode($response, true);
+        
+        return $response["data"];
+
+    }
+
+
+    
+}
+
+
+
+?>
